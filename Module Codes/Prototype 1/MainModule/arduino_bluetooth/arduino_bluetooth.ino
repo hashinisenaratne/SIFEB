1d1aa44e8ca2a2f128bd1ee6e367a21b8f0bd1bc
Hashini Senaratne <hashz1990@gmail.com>
2014-09-11 09:42:16 +0530

    Prototype 1 - Final Module Codes

--- Module Codes/Prototype 1/MainModule/arduino_bluetooth/arduino_bluetooth.ino
+++ Module Codes/Prototype 1/MainModule/arduino_bluetooth/arduino_bluetooth.ino
@@ -1,4 +1,8 @@
 #include <Wire.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(10, 11); // RX, TX
+
 /*
 
 LIST OF COMMANDS
@@ -18,16 +22,17 @@ int cPin = 13;
 
 void setup() {
   Wire.begin();
-  Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
+  mySerial.begin(9600);
   pinMode(dPin, OUTPUT);
   pinMode(cPin, OUTPUT);
 }
 
 void loop() {
+  
   // send data only when you receive data:
-  if (Serial.available() > 0) {
+  if (mySerial.available() > 0) {
     // read the incoming byte:
-    incomingByte = Serial.read();
+    incomingByte = mySerial.read();
 
     // say what you got:
     //Serial.print("I received: ");
@@ -36,48 +41,48 @@ void loop() {
     if(incomingByte == 'a'){  
       Wire.beginTransmission(10); // transmit to device #11
       Wire.write('s'); 
-      Serial.println(Wire.endTransmission());    // stop transmitting
+      mySerial.println(Wire.endTransmission());    // stop transmitting
     }
 
     else if(incomingByte == 'b'){
       Wire.beginTransmission(10); // transmit to device #10
       Wire.write('t'); 
       Wire.write(1); 
-      Serial.println(Wire.endTransmission());    // stop transmitting
+      mySerial.println(Wire.endTransmission());    // stop transmitting
     }
 
     else if(incomingByte == 'c'){
       Wire.beginTransmission(10); // transmit to device #10
       Wire.write('t'); 
       Wire.write(2); 
-      Serial.println(Wire.endTransmission());    // stop transmitting
+      mySerial.println(Wire.endTransmission());    // stop transmitting
     }
     
     else if(incomingByte == 'd'){
       Wire.beginTransmission(10); // transmit to device #10
       Wire.write('t'); 
       Wire.write(3); 
-      Serial.println(Wire.endTransmission());    // stop transmitting
+      mySerial.println(Wire.endTransmission());    // stop transmitting
     }
     
     else if(incomingByte == 'e'){
       Wire.beginTransmission(10); // transmit to device #10
       Wire.write('t'); 
       Wire.write(4); 
-      Serial.println(Wire.endTransmission());    // stop transmitting
+      mySerial.println(Wire.endTransmission());    // stop transmitting
     }
     
     else if(incomingByte == 'f'){
       Wire.beginTransmission(10); // transmit to device #10
       Wire.write('t'); 
       Wire.write(5); 
-      Serial.println(Wire.endTransmission());    // stop transmitting
+      mySerial.println(Wire.endTransmission());    // stop transmitting
     }
     
     if(incomingByte == 'g'){  
       Wire.beginTransmission(11); // transmit to device #11
       Wire.write('s'); 
-      Serial.println(Wire.endTransmission());    // stop transmitting
+      mySerial.println(Wire.endTransmission());    // stop transmitting
     }
     
     else if(incomingByte == 'h'){
@@ -90,12 +95,17 @@ void loop() {
       while(Wire.available())    // slave may send less than requested
       { 
         int c = Wire.read();       // receive a byte
-        Serial.println(c);         // print the character (PC needs to read)
+        mySerial.println(c);         // print the character (PC needs to read)
       }
+    }
+    else if(incomingByte == 'z'){
+      Wire.beginTransmission(10); // go foward
+      Wire.write('a'); 
+      Wire.write(1); 
+      Wire.endTransmission();
     }
     
     else if(incomingByte == 'r'){
-      
       
       delay(2000);
       
@@ -103,7 +113,7 @@ void loop() {
       Wire.write('a'); 
       Wire.write(1); 
       Wire.endTransmission(); 
-
+      mySerial.println("go");
       boolean close = false;
       while(!close){
         Wire.requestFrom(11, 1);    // request 6 bytes from slave device #11
@@ -112,28 +122,27 @@ void loop() {
             int cm = Wire.read();       // receive a byte
             if(cm < 100){
               close = true;
-
+              mySerial.println("sensed");
             }
           }
-
+          mySerial.println("hr");
       }
       
       delay(500);
       
-
+      mySerial.println("beforestop");
       
   
       Wire.beginTransmission(10); // stop
       Wire.write('a'); 
       Wire.write(5); 
-
-      Wire.endTransmission();
+      mySerial.print("stop state");
+      mySerial.println(Wire.endTransmission());
       
       Wire.beginTransmission(10); // turn right
       Wire.write('a'); 
       Wire.write(3); 
       Wire.endTransmission();
-      
     }
 
     else if(incomingByte == '\r'){
