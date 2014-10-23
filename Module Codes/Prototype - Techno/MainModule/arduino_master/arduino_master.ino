055f293a077da15fc6af3175f2b1f1b077d56284
pubudu538 <pubudu538@gmail.com>
2014-10-23 15:26:56 +0530

    adding plug and play change to code

--- Module Codes/Prototype - Techno/MainModule/arduino_master/arduino_master.ino
+++ Module Codes/Prototype - Techno/MainModule/arduino_master/arduino_master.ino
@@ -3,16 +3,16 @@
 
 /*
 
-LIST OF COMMANDS
-----------------
-a - show
-b - forward
-c - reverse
-d - right
-e - left
-f - stop
-
-*/
+ LIST OF COMMANDS
+ ----------------
+ a - show
+ b - forward
+ c - reverse
+ d - right
+ e - left
+ f - stop
+ 
+ */
 
 char incomingByte = 0;   // for incoming serial data
 int dPin = 12;
@@ -23,29 +23,40 @@ unsigned long lastSec=0;
 
 //auto detection support variables
 //edit according to the devices
-int addresses[] = {10, 11};
-boolean lastState[] = {false, false};
+int addresses[] = {
+  10, 11};
+boolean lastState[] = {
+  false, false};
 
 void setup() {
   Wire.begin();
   Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
   pinMode(dPin, OUTPUT);
   pinMode(cPin, OUTPUT);
-  
+
 }
 
 void notifyAllChanges(void)
 {
   //Serial.println("Checking...");
+  boolean isChanged=false;
   for(int i=0; i< sizeof(addresses);i++)
   {
-    notifySingleChange(i);
+    boolean tmp =  notifySingleChange(i);
+    if(tmp)
+    {
+      isChanged=true;
+    }
   }
-  Serial.println("##");
+
+  if(isChanged)
+  {
+    Serial.println("##");
+  }
 }
 
 //print two bytes, c or d & address as a byte followed by a new line.
-void notifySingleChange(int addressIndex)
+boolean notifySingleChange(int addressIndex)
 {
   Wire.beginTransmission(addresses[addressIndex]);
   if(Wire.endTransmission()==0)
@@ -55,6 +66,7 @@ void notifySingleChange(int addressIndex)
       Serial.print('c');
       lastState[addressIndex] = true;
       Serial.println((char)addresses[addressIndex]);
+      return true;
     }
   }
   else
@@ -64,20 +76,22 @@ void notifySingleChange(int addressIndex)
       lastState[addressIndex] = false;
       Serial.print('d');
       Serial.println((char)addresses[addressIndex]);
+      return true;
     }
   }
+  return false;
 }
 
 
 void loop() {
-  
-  
-  /*if(millis()-lastSec >= 5000)
+
+
+  if(millis()-lastSec >= 5000)
   {
     lastSec = millis();
     notifyAllChanges();
-  }*/
-  
+  }
+
   // send data only when you receive data:
   if (Serial.available() > 0) {
     // read the incoming byte:
@@ -90,60 +104,68 @@ void loop() {
     if(incomingByte == 'z'){  
       notifyAllChanges();
     }
-    
+
     if(incomingByte == 'a'){  
       Wire.beginTransmission(10); // transmit to device #11
       Wire.write('s'); 
-      Serial.println(Wire.endTransmission());    // stop transmitting
+      Serial.print(Wire.endTransmission());    // stop transmitting
+      Serial.println("#");
     }
 
     else if(incomingByte == 'b'){
       Wire.beginTransmission(10); // transmit to device #10
       Wire.write('t'); 
       Wire.write(1); 
-      Serial.println(Wire.endTransmission());    // stop transmitting
+      Serial.print(Wire.endTransmission());    // stop transmitting
+      Serial.println("#");
     }
 
     else if(incomingByte == 'c'){
       Wire.beginTransmission(10); // transmit to device #10
       Wire.write('t'); 
       Wire.write(2); 
-      Serial.println(Wire.endTransmission());    // stop transmitting
+      Serial.print(Wire.endTransmission());    // stop transmitting
+      Serial.println("#");
     }
-    
+
     else if(incomingByte == 'd'){
       Wire.beginTransmission(10); // transmit to device #10
       Wire.write('t'); 
       Wire.write(3); 
-      Serial.println(Wire.endTransmission());    // stop transmitting
+      Serial.print(Wire.endTransmission());    // stop transmitting
+      Serial.println("#");
     }
-    
+
     else if(incomingByte == 'e'){
       Wire.beginTransmission(10); // transmit to device #10
       Wire.write('t'); 
       Wire.write(4); 
-      Serial.println(Wire.endTransmission());    // stop transmitting
+      Serial.print(Wire.endTransmission());    // stop transmitting
+      Serial.println("#");
     }
-    
+
     else if(incomingByte == 'f'){
       Wire.beginTransmission(10); // transmit to device #10
       Wire.write('t'); 
       Wire.write(5); 
-      Serial.println(Wire.endTransmission());    // stop transmitting
+      Serial.print(Wire.endTransmission());    // stop transmitting
+      Serial.println("#");
     }
-    
+
     if(incomingByte == 'g'){  
       Wire.beginTransmission(11); // transmit to device #11
       Wire.write('s'); 
-      Serial.println(Wire.endTransmission());    // stop transmitting
+      Serial.print(Wire.endTransmission());    // stop transmitting
+      Serial.println("#");
     }
-    
+
     else if(incomingByte == 'h'){
       /*Wire.beginTransmission(11); // transmit to device #11
-      Wire.write('t'); 
-      Wire.write(1); 
-      Serial.println(Wire.endTransmission());    // stop transmitting
-      */
+       Wire.write('t'); 
+       Wire.write(1); 
+       Serial.print(Wire.endTransmission());    // stop transmitting
+       Serial.println("#");
+       */
       Wire.requestFrom(11, 1);    // request 1 byte from slave device #11
       while(Wire.available())    // slave may send less than requested
       { 
@@ -151,12 +173,12 @@ void loop() {
         Serial.println(c);         // print the character (PC needs to read)
       }
     }
-    
+
     else if(incomingByte == 'r'){
-      
-      
+
+
       delay(2000);
-      
+
       Wire.beginTransmission(10); // go foward
       Wire.write('a'); 
       Wire.write(1); 
@@ -165,33 +187,33 @@ void loop() {
       boolean close = false;
       while(!close){
         Wire.requestFrom(11, 1);    // request 6 bytes from slave device #11
-          while(Wire.available())    // slave may send less than requested
-          { 
-            int cm = Wire.read();       // receive a byte
-            if(cm < 100){
-              close = true;
+        while(Wire.available())    // slave may send less than requested
+        { 
+          int cm = Wire.read();       // receive a byte
+          if(cm < 100){
+            close = true;
 
-            }
           }
+        }
 
       }
-      
-      delay(500);
-      
 
-      
-  
+      delay(500);
+
+
+
+
       Wire.beginTransmission(10); // stop
       Wire.write('a'); 
       Wire.write(5); 
 
       Wire.endTransmission();
-      
+
       Wire.beginTransmission(10); // turn right
       Wire.write('a'); 
       Wire.write(3); 
       Wire.endTransmission();
-      
+
     }
 
     else if(incomingByte == '\r'){
@@ -200,6 +222,7 @@ void loop() {
 
   }
 }
+
 
 
 
