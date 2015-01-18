8792bfee063af67c89ca66c4f778e54463767596
Hashini Senaratne <hashz1990@gmail.com>
2015-01-18 13:35:47 +0530

    LCD testing

--- Testing/LCD/LCD/LCD.ino
+++ Testing/LCD/LCD/LCD.ino
@@ -1,5 +1,5 @@
 /*
-This code is to test the lcd connected to the main module cct as at 02.01.2015
+This code is to test the lcd connected to the main module cct as at 17.01.2015
  
   The circuit:
  * LCD RS pin to digital pin 9
@@ -11,27 +11,54 @@ This code is to test the lcd connected to the main module cct as at 02.01.2015
  * LCD back light pin to digital pin 6
  * LCD R/W pin to ground
  */
+ 
+#define LCD_REGISTERSELECT 9
+#define LCD_ENABLE 8
+#define LCD_DATA4 5
+#define LCD_DATA5 4
+#define LCD_DATA6 3
+#define LCD_DATA7 2
+#define LCD_BACKLIGHT 6
 
 // include the library code:
 #include <LiquidCrystal.h>
 
 // initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(9, 8, 5, 4, 3, 2);
+LiquidCrystal lcd(LCD_REGISTERSELECT, LCD_ENABLE, LCD_DATA4, LCD_DATA5, LCD_DATA6, LCD_DATA7);
+byte lcd_error = 0;
 
 void setup() {
-  pinMode(6,OUTPUT);
-  digitalWrite(6,HIGH);
+  pinMode(LCD_BACKLIGHT,OUTPUT);
+  digitalWrite(LCD_BACKLIGHT,HIGH);
   // set up the LCD's number of columns and rows: 
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("hello, world!");
+  lcdMessage(0,"     SiFEB     ",0);
 }
 
 void loop() {
+  lcdMessage(1,"Program Running",0);
+  delay(10000);
+  
+  lcdMessage(1,"     ERROR     ",1);  
+  
+  // print the number of seconds since reset:
+  //lcd.print(millis()/1000);
+}
+
+void lcdMessage(int row, char message[], byte error){
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(millis()/1000);
+  lcd.setCursor(0, row);
+  digitalWrite(LCD_BACKLIGHT,HIGH);
+  lcd.print(message);
+  if(error == 1){
+    while (true) {
+      digitalWrite(LCD_BACKLIGHT,HIGH);
+      delay(500);
+      digitalWrite(LCD_BACKLIGHT,LOW);
+      delay(500);
+    }
+  }
 }
 
